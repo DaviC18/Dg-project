09d9d59aeb193c2f1e83eb3bac4b1fce799f7dd5
Davi Curty <davicurty850@gmail.com>
2026-06-10 23:00:12 -0300

    test: add validation for consent in create form tests

--- server/src/tests/form/create-form.spec.ts
+++ server/src/tests/form/create-form.spec.ts
@@ -55,4 +55,16 @@ describe("Create Form", () => {
         expect(body.form.userId).toBe("test-user-id");
         expect(body.form.symptomsDescription).toBe("Dor de cabeça");
     })
+    
+    it("should return 400 if consent is not given", async () => {
+        const response = await app.inject({
+            method: "POST", 
+            url: "/pre-diagnostics",
+            payload: {
+                formId: "some-form-id",
+            }
+        })
+        expect(response.statusCode).toBe(400);
+        expect(response.json()).toEqual({err: "Error to create the Form"})
+    })
 })
